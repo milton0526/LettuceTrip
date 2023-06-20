ba67849b3e90311bc5dc73115eb8c9f4cb8d306d
Milton Liu <milton562349@gmail.com>
2023-06-20 17:56:52 +0800

    Initial Commit

---
+++ LettuceTrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LettuceTrip
+//
+//  Created by Milton Liu on 2023/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
